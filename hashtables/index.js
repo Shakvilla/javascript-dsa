ea881109ca9c4a1083af7222f217dac0cefad6ed
Shakvilla <abdulshakuraclement@yahoo.com>
2023-05-06 10:53:10 +0000

    creating hashtables

---
+++ hashtables/index.js
@@ -0,0 +1,29 @@
+function firstRecurringCharacter(input) {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      if (input[i] === input[j]) {
+        return input[i];
+      }
+    }
+  }
+
+  return undefined;
+} // O(n^2)
+
+firstRecurringCharacter([1, 2, 3, 4, 5, 1]);
+
+function firstRecurringCharacter2(input) {
+  let map = {};
+  for (let i = 0; i < input.length; i++) {
+    console.log(map[2]);
+    if (map[input[i]]) {
+      return input[i];
+    } else {
+      map[input[i]] = i;
+    }
+  }
+
+  return undefined;
+}
+
+firstRecurringCharacter2([2, 5, 1, 2, 4, 3, 2]);
