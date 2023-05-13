c9ab9f8d91dbee303d090fda989de8072a2232c6
Shakvilla <abdulshakuraclement@yahoo.com>
2023-05-13 09:20:51 +0000

    implemented queue

--- stacks/stack-arrays.js
+++ stacks/stack-arrays.js
@@ -6,6 +6,16 @@ class Stack {
   peek() {
     return this.array[this.array.length - 1];
   }
+
+  push(value) {
+    this.array.push(value);
+    return this;
+  }
+
+  pop() {
+    this.array.pop();
+    return this;
+  }
 }
 
 const myStack = new Stack();
