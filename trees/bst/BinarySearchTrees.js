267930a6e7ff22c87cdf93f7411e97615a48a67b
Shakvilla <abdulshakuraclement@yahoo.com>
2023-05-14 17:11:46 +0000

    implementing bst

---
+++ trees/bst/BinarySearchTrees.js
@@ -0,0 +1,18 @@
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {}
+  lookup(value) {}
+}
+
+const tree = new BinarySearchTree();
