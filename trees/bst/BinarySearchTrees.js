7caa9ffd6981f182474d8fa3c7cb15a7b50914a1
Shakvilla <abdulshakuraclement@yahoo.com>
2023-05-14 18:14:12 +0000

    insert method implemented

--- trees/bst/BinarySearchTrees.js
+++ trees/bst/BinarySearchTrees.js
@@ -11,7 +11,39 @@ class BinarySearchTree {
     this.root = null;
   }
 
-  insert(value) {}
+  /**
+   * 1. create a new node and assign it to newNode
+   * 2. check if tree is empty, assign newNode as root
+   * 3. if tree is not empty, assign current node as root
+   * 4. run a while loop, when value is less than currentNode insert it at the left of the parent node
+   * 5. if it's greater or equal to currentNode, insert it at the right of parent node
+   *
+   */
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          // go left
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          // go right
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
   lookup(value) {}
 }
 
